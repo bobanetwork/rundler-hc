cfd52cc497a8a4fded36ecc44bde8f243d0d7fa0
Michael Montour <mmontour@enya.ai>
2024-08-27 13:56:46 -0700

    Fix a port 8080 conflict with the local devnet.  Changes to be committed: 	modified:   hybrid-compute/runit.sh

--- hybrid-compute/runit.sh
+++ hybrid-compute/runit.sh
@@ -4,5 +4,6 @@
 RUST_BACKTRACE=1 ETH_POLL_INTERVAL_MILLIS=5000 \
   ../target/debug/rundler node \
   --rpc.port 3300 \
+  --metrics.port 8380 \
   --builder.private_key $BUILDER_PRIVKEY \
   $@ 2>&1
