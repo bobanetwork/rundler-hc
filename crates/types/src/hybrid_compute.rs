357ff33a870f92fc1f11d16c055650c409534b04
Michael Montour <mmontour@enya.ai>
2024-09-03 15:30:36 -0700

    Revert "WORKAROUND: use U256 datatype for StateDiff, for compatibility with"
    This reverts commit a6c7391aa38be1253a7af8f31a85c3c322b8a2b4.
    
     Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   crates/rpc/src/eth/api.rs
    	modified:   crates/types/src/hybrid_compute.rs

--- crates/types/src/hybrid_compute.rs
+++ crates/types/src/hybrid_compute.rs
@@ -438,8 +438,7 @@ pub fn get_hc_op_statediff(op_hash: H256, mut s2: ethers::types::spoof::State) -
     let cfg = HC_CONFIG.lock().unwrap();
 
     // Store an encoded length for the response bytes
-    //let val = H256::from_low_u64_be((payload.len() * 2 + 1).try_into().unwrap());
-    let val = U256::from(payload.len() * 2 + 1);
+    let val = H256::from_low_u64_be((payload.len() * 2 + 1).try_into().unwrap());
     //println!("HC Store1 {:?} {:?}", key, val);
 
     s2.account(cfg.helper_addr).store(key, val);
@@ -447,7 +446,7 @@ pub fn get_hc_op_statediff(op_hash: H256, mut s2: ethers::types::spoof::State) -
 
     let mut i = 0;
     while i < payload.len() {
-	let next_chunk:U256 = H256::from_slice(&payload[i..32+i]).into_uint();
+	let next_chunk:H256 = H256::from_slice(&payload[i..32+i]);
 	//println!("HC Store_next {:?} {:?}", key , next_chunk);
 	s2.account(cfg.helper_addr).store(key, next_chunk);
 	let u_key:U256 = key.into_uint()+1;
