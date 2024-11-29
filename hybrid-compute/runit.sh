52b81ae5f9b5b035161fa0d093b4a7721086e3a5
Michael Montour <mmontour@enya.ai>
2024-11-29 12:47:13 -0800

    fix: Remove old BUILDER_PRIVKEY variables, merge into BUILDER_PRIVATE_KEYS  Changes to be committed: 	modified:   local.env 	modified:   runit.sh

--- hybrid-compute/runit.sh
+++ hybrid-compute/runit.sh
@@ -5,6 +5,5 @@ RUST_BACKTRACE=1 ETH_POLL_INTERVAL_MILLIS=5000 \
   ../target/debug/rundler node \
   --rpc.port 3300 \
   --metrics.port 8380 \
-  --builder.private_keys $BUILDER_PRIVKEY_V6,$BUILDER_PRIVKEY \
   --builder.dropped_status_unsupported \
   $@ 2>&1
