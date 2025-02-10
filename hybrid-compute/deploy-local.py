6b1e902b416b887d994b3646dc21e8b80194410d
Michael Montour <mmontour@enya.ai>
2025-02-10 13:53:23 -0800

    fix: move VRF key out of deploy-local and into local.env template.
     Changes to be committed:
    	modified:   hybrid-compute/deploy-local.py
    	modified:   hybrid-compute/local.env

--- hybrid-compute/deploy-local.py
+++ hybrid-compute/deploy-local.py
@@ -308,15 +308,11 @@ def deploy_base():
     print("Deployed base contracts:", addrs)
     return addrs.split(',')
 
-# Used for HC_VRF inside TestHybrid
-oc_random_secret  = "0x091bda8b27b681ee8de4d0c2e6743e1339b986e22041a2de2cd5d57857a77d65"
-oc_random_keyhash = "0x42856baa9213a4e84d230e72c7a67ed6627d8872cc2f3cfcd4982b90ede8b70d"
-
 def deploy_examples(hybrid_acct_addr):
     cmd_env = {}
     cmd_env['OC_HYBRID_ACCOUNT'] = hybrid_acct_addr
     cmd_env['BOBA_TOKEN'] = boba_token
-    cmd_env['OC_RANDOM_KEYHASH'] = oc_random_keyhash
+    cmd_env['OC_RANDOM_KEYHASH'] = env_vars['OC_RANDOM_KEYHASH']
     if ep7:
         addrs = deploy_forge("hc_scripts/ExampleDeploy_v7.s.sol", cmd_env)
     else:
@@ -488,8 +484,6 @@ env_vars['TEST_RANDOM'] = TEST_RANDOM.address
 # Other
 env_vars['BOBA_TOKEN'] = boba_token
 env_vars['SIMPLE_PM'] = pm_addr
-env_vars['OC_RANDOM_SECRET']  = oc_random_secret
-env_vars['OC_RANDOM_KEYHASH'] = oc_random_keyhash
 env_vars['OC_NODE_HTTP'] = env_vars['NODE_HTTP']
 
 with open(".env", "w", encoding="ascii") as f:
