2c06c519001ab979a1ffdc34fd3ce292c3d56c82
Michael Montour <mmontour@enya.ai>
2025-10-09 23:12:04 -0700

    fix: wait for blocks to be mined before starting fee_increase. Changes to be committed: 	modified:   runit.sh

--- hybrid-compute/runit.sh
+++ hybrid-compute/runit.sh
@@ -3,11 +3,13 @@
 # Configuration parameters are now taken from environment variables.
 # Changes here should be synced with ../docker-wrapper.sh
 
-RUST_BACKTRACE=1 ETH_POLL_INTERVAL_MILLIS=5000 \
+RUST_BACKTRACE=1 \
   ../target/debug/rundler node \
-  --rpc.port 3300 \
-  --metrics.port 8380 \
+  --signer.redis_uri=redis://127.0.0.1:6379 \
   --min_stake_value 1000000000000000 \
   --min_unstake_delay 60 \
-  --signer.redis_uri=redis://127.0.0.1:6379 \
+  --rpc.port 3300 \
+  --metrics.port 8380 \
+  --pool.chain_poll_interval_millis 500 \
+  --builder.max_blocks_to_wait_for_mine 8 \
   $@ 2>&1
